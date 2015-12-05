c7ac9ae69404cee25c6fb8a860db7defdcc10e5d
saxer <15201280641@qq.com>
2015-12-05 09:50:11 +0800

    no message

--- test.rb
+++ test.rb
@@ -1,24 +1,32 @@
-jp_str = "あ ア 歲 い イ i う ウ 18 え エ 性 お オ 愛
+class RN
+  jp_str = "あ ア 歲 い イ i う ウ 18 え エ 性 お オ 愛
 
 か カ 寞 き キ 寂 く ク 熱 け ケ 看 こ コ av 學 心 涩
 
-さ サ 林 し シ 動 す ス 京 せ 力 セ 女 そ ソ 少 美 初
+さ サ 林 し シ 動 す 交 ス 水 京 せ 力 セ 女 そ ソ 少 淫 美 初
 
-た タ 空 ち チ chi つ ツ 日 て ° 度 テ 影 と ト 儿 愛 16
+た タ 空 ち チ chi つ ツ 日 て ° 度 夜 テ 漢 影 と ト 儿 愛 16
 
-な ナ 倉 に ニ 性 ぬ 電 ヌ 本 ね ネ  の ノ 図 師 卯 20
+な ナ 倉 に 雨 ニ 性 ぬ 電 ヌ 本 ね 濕 ネ  の ノ 図 師 卯 20
 
-は ハ 爱 ひ ヒ 澤 夫 ふ フ 国 へ ヘ 妹 愛 爱 艾 ほ ホ 人 啦 妻
+は ハ 爱 ひ ヒ 胸 澤 夫 ふ フ 国 へ 友 ヘ 妹 愛 爱 艾 ほ ホ 人 啦 妻
 
-ま マ 龍 み ミ 島 む ム 天 影 め メ O も モ 成 人 情
+ま マ 龍 み ミ 島 む ム 天 影 め メ O 乳 も モ 成 人 情
 
 や ヤ の ゆ ユ yu よ ヨ 播 情 銫 愛 O X X 力 娜
 
 ら ラ 城 り リ ri る ル ru れ レ 仁 ろ ロ 仆 试
 
-わ ワ 野 を ヲ 郎 生
+わ ワ 野 を ヲ 郎 生 口
 
 ん ン n 美 女 直 播 秀 小 電 影 空 色 愛 情 羞 銫 君 と 二 人 で "
-array = jp_str.split(" ")
+  ARRAY = jp_str.split(" ")
 
-puts (0..(2+rand(4))).to_a.inject('') { |sum,i| sum+=array[rand(array.length)] }
+  def random_name
+    puts (1..(2+rand(4))).to_a.inject('') { |sum, i| sum+=ARRAY[rand(ARRAY.length)] }
+  end
+
+end
+
+rm = RN.new
+20.times.each { rm.random_name }
